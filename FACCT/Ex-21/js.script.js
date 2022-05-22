bd6542195ff8b479b4fa74f6e0bfef4f306e93f7
Arthur Ferrari <101263714+Arthcode08@users.noreply.github.com>
2022-05-22 18:45:47 -0300

    Add files via upload

---
+++ FACCT/Ex-21/js.script.js
@@ -0,0 +1,11 @@
+let inicioJogo, fimJogo, horas
+
+inicioJogo = parseInt(prompt("Que horas começou o jogo ? "))
+
+fimJogo = parseInt(prompt("Que horas acabou o Jogo ? "))
+
+if (fimJogo >= inicioJogo) {
+    horas = fimJogo-inicioJogo;
+} else {
+    horas = (24-inicioJogo)+fimJogo;
+}
